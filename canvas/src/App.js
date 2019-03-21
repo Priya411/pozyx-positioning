b8d01f3c148072a8b6e89799aec8afe13688c2f3
Nikita <n.sumahers@gmail.com>
2019-03-21 13:21:32 +0100

    new module hack

--- canvas/src/App.js
+++ canvas/src/App.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import RCanvas from '../components/RainbowCanvas';
-
+import CanvasDraw from './components/CanvasDraw';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <RCanvas/>
-      </div>
-    );
-  }
+    render() {
+        
+        let vw = window.innerWidth;
+        let vh = window.innerHeight;
+        
+        return (
+                <div className="App">
+                    <CanvasDraw canvasWidth={vw} canvasHeight={vh}/>
+                </div>
+                );
+    }
 }
 
 export default App;
