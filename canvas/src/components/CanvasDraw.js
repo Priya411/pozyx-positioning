9bfa30e607f05cf7c7d66163dee051b6b7ac557b
Sarmad <sarmadsa2@gmail.com>
2019-03-21 17:27:12 +0100

    Recommit

--- canvas/src/components/CanvasDraw.js
+++ canvas/src/components/CanvasDraw.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 
 class CanvasDraw extends Component {
 
@@ -23,12 +23,15 @@ class CanvasDraw extends Component {
         this.toggleControls = this.toggleControls.bind(this);
         this.handleInputChange = this.handleInputChange.bind(this);
     }
+
     canvas() {
         return document.querySelector("#draw");
     }
+
     ctx() {
         return this.canvas().getContext("2d");
     }
+
     componentDidMount() {
         const canvas = this.canvas()
         const ctx = this.ctx()
@@ -41,14 +44,14 @@ class CanvasDraw extends Component {
         ctx.lineCap = "round";
         ctx.lineWidth = Number(this.state.minWidth) + 1;
         this.drawLoop();
-       
+
     }
-    
+
     drawLoop = () => {
         this.draw();
         window.requestAnimationFrame(this.drawLoop);
     }
-    
+
     draw(e) {
         const ctx = this.ctx();
         let hue = this.state.hue;
@@ -78,6 +81,7 @@ class CanvasDraw extends Component {
 
         }
     }
+
     handleWidth(e) {
         const ctx = this.canvas().getContext("2d");
         let nextState = this.state.direction;
@@ -93,15 +97,16 @@ class CanvasDraw extends Component {
             ctx.lineWidth--
         }
     }
+
     toggleControls() {
         let onScreen = this.state.controlLeft;
         let display = this.state.controlDisplay;
         const fade = () => {
             onScreen === "0%" ? (
-                    this.setState({controlLeft: "100%"})
-                    ) : (
-                    this.setState({controlLeft: "0%"})
-                    )
+                this.setState({controlLeft: "100%"})
+            ) : (
+                this.setState({controlLeft: "0%"})
+            )
         }
         if ((display === "none" && onScreen === "100%") || (display === "block" && onScreen === "0%")) {
             if (display === "none") {
@@ -114,6 +119,7 @@ class CanvasDraw extends Component {
             ;
         }
     }
+
     handleInputChange(event) {
         const target = event.target
         const value = target.type === "checkbox" ? target.checked : target.value
@@ -135,164 +141,166 @@ class CanvasDraw extends Component {
     }
 
     render() {
-        
+
         //console.log(this.state.lastX);
-        
-            
+
+
         const canvasStyle = {
             border: "1px solid black"
         }
 
         return (
-                <div>
-                    <canvas id="draw" width={this.props.width} height={this.props.height}
-                            style={canvasStyle}/>
-                    <Controls left={this.state.controlLeft} display={this.state.controlDisplay} canvas={this.canvas}
-                              ctx={this.ctx} color={this.state.color} customColor={this.state.customColor}
-                              handleInputChange={this.handleInputChange} maxWidth={this.state.maxWidth} minWidth={this.state.minWidth}
-                              fixedWidth={this.state.customStroke}/>
-                    <ButtonOptions onClick={this.toggleControls}/>
-                </div>
-                                )
+            <div>
+                <canvas id="draw" width={this.props.width} height={this.props.height}
+                        style={canvasStyle}/>
+                <Controls left={this.state.controlLeft} display={this.state.controlDisplay} canvas={this.canvas}
+                          ctx={this.ctx} color={this.state.color} customColor={this.state.customColor}
+                          handleInputChange={this.handleInputChange} maxWidth={this.state.maxWidth}
+                          minWidth={this.state.minWidth}
+                          fixedWidth={this.state.customStroke}/>
+                <ButtonOptions onClick={this.toggleControls}/>
+            </div>
+        )
+    }
+}
+
+function ButtonOptions(props) {
+    const buttonStyle = {
+        textAlign: "center",
+        position: "absolute",
+        right: "10px",
+        top: "10px",
+        cursor: "pointer",
+        padding: "8px",
+        color: "white",
+        backgroundColor: "rgb(47, 47, 47)",
+        border: "3px solid red",
+        boxShadow: "1px 1px 5px rgba(0, 0, 0, 0.47)"
+    }
+    return (
+        <div onClick={props.onClick} style={buttonStyle}>
+            <i className="fa fa-cogs" aria-hidden="true"></i>
+        </div>
+    )
+}
+
+function Controls(props) {
+    const container = {
+        position: "absolute",
+        right: "70px",
+        top: "0",
+        backgroundColor: "transparent",
+        width: "400px",
+        height: "150px",
+        overflow: "hidden",
+        borderRadius: "0 0 5px 5px",
+        display: `${props.display}`
+    }
+    const content = {
+        backgroundColor: "rgb(47, 47, 47)",
+        color: "white",
+        boxSizing: "border-box",
+        boxShadow: "rgba(0, 0, 0, 0.28) 0px 1px 2px 2px",
+        fontFamily: "sans-serif",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        width: "100%",
+        height: "100%",
+        padding: "10px",
+        borderRadius: "0 0 5px 5px",
+        position: "absolute",
+        left: `${props.left || 0}`,
+        transition: "0.5s cubic-bezier(0.22, 0.61, 0.36, 1)"
+    }
+    return (
+        <div style={container}>
+            <div style={content}>
+                <StrokeCheckbox checked={props.customWidth} handleChange={props.handleInputChange}/>
+                <StrokeWidth minWidth={props.minWidth} maxWidth={props.maxWidth}
+                             fixedWidth={props.fixedWidth} handleChange={props.handleInputChange}/>
+                <ColorCheckbox checked={props.customColor} handleChange={props.handleInputChange}/>
+                {props.customColor &&
+                <ColorPicker color={props.color} handleChange={props.handleInputChange}/>
                 }
-            }
-            
-            function ButtonOptions (props) {
-                const buttonStyle = {
-                    textAlign: "center",
-                    position: "absolute",
-                    right: "10px",
-                    top: "10px",
-                    cursor: "pointer",
-                    padding: "8px",
-                    color: "white",
-                    backgroundColor: "rgb(47, 47, 47)",
-                    border: "3px solid red",
-                    boxShadow: "1px 1px 5px rgba(0, 0, 0, 0.47)"
-                    }
-  return (
-    <div onClick={props.onClick} style={buttonStyle}>
-      <i className="fa fa-cogs" aria-hidden="true"></i>
-    </div>
-  )
+                <ClearCanvas ctx={props.ctx} canvas={props.canvas}/>
+            </div>
+        </div>
+    )
 }
 
-function Controls (props) {
-  const container = {
-    position: "absolute",
-    right: "70px",
-    top: "0",
-    backgroundColor: "transparent",
-    width: "400px",
-    height: "150px",
-    overflow: "hidden",
-    borderRadius: "0 0 5px 5px",
-    display: `${props.display}`
-  }
-  const content = {
-    backgroundColor: "rgb(47, 47, 47)",
-    color: "white",
-    boxSizing: "border-box",
-    boxShadow: "rgba(0, 0, 0, 0.28) 0px 1px 2px 2px",
-    fontFamily: "sans-serif",
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "space-between",
-    width: "100%",
-    height: "100%",
-    padding: "10px",
-    borderRadius: "0 0 5px 5px",
-    position: "absolute",
-    left: `${props.left || 0}`,
-    transition: "0.5s cubic-bezier(0.22, 0.61, 0.36, 1)"
-  }
-  return (
-    <div style={container}>
-      <div style={content}>
-        <StrokeCheckbox checked={props.customWidth} handleChange={props.handleInputChange} />
-        <StrokeWidth minWidth={props.minWidth} maxWidth={props.maxWidth}
-        fixedWidth={props.fixedWidth} handleChange={props.handleInputChange} />
-        <ColorCheckbox checked={props.customColor} handleChange={props.handleInputChange}/>
-        {props.customColor &&
-          <ColorPicker color={props.color} handleChange={props.handleInputChange}/>
-        }
-        <ClearCanvas ctx={props.ctx} canvas={props.canvas}/>
-      </div>
-    </div>
-  )
+function ClearCanvas(props) {
+    const buttonStyle = {
+        display: "block",
+        float: "right",
+        border: "1px solid #840000",
+        backgroundColor: "#ff2929",
+        cursor: "pointer"
+    }
+    const clear = () => {
+        props.ctx().clearRect(0, 0, props.canvas().width, props.canvas().height)
+    }
+    return (
+        <button style={buttonStyle} onClick={clear}>Clear Canvas</button>
+    )
 }
 
-function ClearCanvas (props) {
-  const buttonStyle = {
-    display: "block",
-    float: "right",
-    border: "1px solid #840000",
-    backgroundColor: "#ff2929",
-    cursor: "pointer"
-  }
-  const clear = () => {
-    props.ctx().clearRect(0, 0, props.canvas().width, props.canvas().height)
-  }
-  return (
-    <button style={buttonStyle} onClick={clear}>Clear Canvas</button>
-  )
+function ColorCheckbox(props) {
+    return (
+        <div>
+            <label>
+                <input name="customColor" type="checkbox" onChange={props.handleChange} value={props.checked}/>
+                Custom Color
+            </label>
+        </div>
+    )
 }
 
-function ColorCheckbox (props) {
-  return (
-    <div>
-      <label>
-        <input name="customColor" type="checkbox" onChange={props.handleChange} value={props.checked} />
-        Custom Color
-      </label>
-    </div>
-  )
+function ColorPicker(props) {
+    return (
+        <input name="color" type="color" onChange={props.handleChange} value={props.color}/>
+    )
 }
 
-function ColorPicker (props) {
-  return (
-    <input name="color" type="color" onChange={props.handleChange} value={props.color} />
-  )
-}
-function StrokeCheckbox (props) {
-  return (
-    <div>
-      <label>
-        <input name="customStroke" type="checkbox" onChange={props.handleChange} value={props.checked} />
-        Fixed Stroke Width
-      </label>
-    </div>
-  )
+function StrokeCheckbox(props) {
+    return (
+        <div>
+            <label>
+                <input name="customStroke" type="checkbox" onChange={props.handleChange} value={props.checked}/>
+                Fixed Stroke Width
+            </label>
+        </div>
+    )
 }
 
-function StrokeWidth (props) {
-  const strokeControlStyle = {
-    display: "flex",
-    flexDirection: "row"
-  }
-  const inputStyle = {
-    display: "block"
-  }
-  return (
-    <div style={strokeControlStyle}>
-      <label>
-      {props.fixedWidth ? "Fixed" : "min"} Stroke Width
-      <input style={inputStyle} name="minWidth" type="range"
-        onChange={props.handleChange} value={props.minWidth}
-        min="1" max="150" step="1"
-      />
-      </label>
-      {!props.fixedWidth && (
-        <label>
-        max Stroke Width
-        <input style={inputStyle} name="maxWidth" type="range"
-          onChange={props.handleChange} value={props.maxWidth}
-          min="1" max="150" step="1"
-        />
-        </label>
-      )}
-    </div>
-  )
+function StrokeWidth(props) {
+    const strokeControlStyle = {
+        display: "flex",
+        flexDirection: "row"
+    }
+    const inputStyle = {
+        display: "block"
+    }
+    return (
+        <div style={strokeControlStyle}>
+            <label>
+                {props.fixedWidth ? "Fixed" : "min"} Stroke Width
+                <input style={inputStyle} name="minWidth" type="range"
+                       onChange={props.handleChange} value={props.minWidth}
+                       min="1" max="150" step="1"
+                />
+            </label>
+            {!props.fixedWidth && (
+                <label>
+                    max Stroke Width
+                    <input style={inputStyle} name="maxWidth" type="range"
+                           onChange={props.handleChange} value={props.maxWidth}
+                           min="1" max="150" step="1"
+                    />
+                </label>
+            )}
+        </div>
+    )
 }
 
-            export default CanvasDraw;
+export default CanvasDraw;
